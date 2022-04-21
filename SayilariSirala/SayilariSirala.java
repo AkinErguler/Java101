939e4db9434c3c8228bc11f23ac11d447af061c9
Nusret Malli <nsrtnn01@gmail>
2022-04-21 07:48:32 +0300

    Sayilari Kucukten Buyuge Siralayan Program Odevi

--- SayilariSirala/SayilariSirala.java
+++ SayilariSirala/SayilariSirala.java
@@ -17,37 +17,37 @@ public class SayilariSirala {
         System.out.println("Üçüncü Sayiyi Giriniz:");
         s3= input.nextInt();
 
-        if(s1>s2 && s1>s3)
+        if(s1<s2 && s1<s3)
         {
-            if ((s2>s3))
+            if ((s2<s3))
             {
-                System.out.println(s1 + ">" + s2 + ">" + s3);
+                System.out.println(s1 + "<" + s2 + "<" + s3);
             }
             else
             {
-                System.out.println(s1 + ">" + s3 + ">" + s2);
+                System.out.println(s1 + "<" + s3 + "<" + s2);
             }
         }
-        else if (s2>s1 && s2>s3)
+        else if (s2<s1 && s2<s3)
         {
-            if ((s1>s3))
+            if ((s1<s3))
             {
-                System.out.println(s2 + ">" + s1 + ">" + s3);
+                System.out.println(s2 + "<" + s1 + "<" + s3);
             }
             else
             {
-                System.out.println(s2 + ">" + s3 + ">" + s1);
+                System.out.println(s2 + "<" + s3 + "<" + s1);
             }
         }
-        else if (s3>s1 && s3>s2)
+        else if (s3<s1 && s3<s2)
         {
-            if ((s3>s1))
+            if ((s1<s2))
             {
-                System.out.println(s3 + ">" + s1 + ">" + s2);
+                System.out.println(s3 + "<" + s1 + "<" + s2);
             }
             else
             {
-                System.out.println(s3 + ">" + s2 + ">" + s1);
+                System.out.println(s3 + "<" + s2 + "<" + s1);
             }
         }
 
